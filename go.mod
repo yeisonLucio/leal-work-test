b4e221252368105419be6df83c6cad1d30113cf9
Yeison Lucio <yeisonlucio1266@gmail.com>
2023-06-30 15:52:23 -0500

    feat: Added tests for create user use case

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -34,9 +35,11 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/redis/go-redis/v9 v9.0.5 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
 	github.com/swaggo/gin-swagger v1.6.0 // indirect
 	github.com/swaggo/swag v1.16.1 // indirect
@@ -62,6 +65,7 @@ require (
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/crypto v0.10.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
 	gorm.io/driver/mysql v1.5.1
---
+++ src/application/usecases/create_user_uc_test.go
@@ -0,0 +1,107 @@
+package usecases
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"lucio.com/order-service/src/domain/dto"
+	"lucio.com/order-service/src/domain/entities"
+	"lucio.com/order-service/src/domain/valueobjects"
+)
+
+func TestCreateUserUC_Execute(t *testing.T) {
+	type fields struct {
+		UserRepository *userRepositoryMock
+	}
+	type args struct {
+		createUserDTO dto.CreateUserDTO
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		mocker  func(a args, f fields)
+		want    *dto.UserCreatedDTO
+		wantErr bool
+	}{
+		{
+			name: "should return an error when user cannot be created",
+			fields: fields{
+				UserRepository: &userRepositoryMock{},
+			},
+			args: args{
+				createUserDTO: dto.CreateUserDTO{},
+			},
+			mocker: func(a args, f fields) {
+				user := entities.User{
+					Status: valueobjects.ActiveStatus,
+				}
+				f.UserRepository.On("Create", user).Return(&user, errors.New("user create error"))
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "should create user successfully",
+			fields: fields{
+				UserRepository: &userRepositoryMock{},
+			},
+			args: args{
+				createUserDTO: dto.CreateUserDTO{
+					Name:           "test",
+					Identification: "123",
+				},
+			},
+			mocker: func(a args, f fields) {
+				user := entities.User{
+					Status:         valueobjects.ActiveStatus,
+					Name:           a.createUserDTO.Name,
+					Identification: a.createUserDTO.Identification,
+				}
+				userResponse := user
+				userResponse.ID = 1
+				f.UserRepository.On("Create", user).Return(&userResponse, nil)
+			},
+			want: &dto.UserCreatedDTO{
+				ID:             1,
+				Name:           "test",
+				Identification: "123",
+				Status:         string(valueobjects.ActiveStatus),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mocker(tt.args, tt.fields)
+			c := &CreateUserUC{
+				UserRepository: tt.fields.UserRepository,
+			}
+			got, err := c.Execute(tt.args.createUserDTO)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateUserUC.Execute() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateUserUC.Execute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// Mocks
+
+type userRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *userRepositoryMock) Create(user entities.User) (*entities.User, error) {
+	args := m.Called(user)
+	return args.Get(0).(*entities.User), args.Error(1)
+}
+
+func (m *userRepositoryMock) FindByID(ID uint) *entities.User {
+	args := m.Called(ID)
+	return args.Get(0).(*entities.User)
+}
